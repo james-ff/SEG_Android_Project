15566ceb6658c6774c085a72e6de0b184e272b3b
James Bellamy <k1209605@kcl.ac.uk>
2013-10-26 21:13:53 +0100

    JSON Core Class

---
+++ Worldly/src/com/example/worldly/JSONCore.java
@@ -0,0 +1,58 @@
+package com.example.worldly;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+
+public class JSONCore {
+	
+	//private final String API_BASE_URL = "http://api.worldbank.org/countries/";
+	
+	public static String readData(String url) {
+	    // Create download objects
+	    HttpClient client = new DefaultHttpClient();
+	    HttpGet get = new HttpGet(url);
+	    StringBuilder content = new StringBuilder();
+	    
+	    try {
+	        // Execute response and create input stream
+	        HttpResponse response = client.execute(get);
+	        int responseCode = response.getStatusLine().getStatusCode();
+	        if (responseCode == 200) {
+	            InputStream in = response.getEntity().getContent();
+	            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+	            
+	            // Build string from input stream
+	            String readLine = reader.readLine();
+	            while (readLine != null) {
+	                content.append(readLine);
+	                readLine = reader.readLine();
+	            }
+	        } else {
+	            Log.w("DATA RETRIEVAL","Unable to read data.  HTTP response code = " + responseCode);
+	            content = null;
+	        }
+	    } catch (ClientProtocolException e) {
+	        Log.e("readData","ClientProtocolException:\n" + e.getMessage());
+	    } catch (IOException e) {
+	        Log.e("readData","IOException:\n" + e.getMessage());
+	    }
+		
+	    // return data
+	    if (content==null) {
+	        return(null);
+	    } else {
+	        return(content.toString());
+	    }
+	}
+}
