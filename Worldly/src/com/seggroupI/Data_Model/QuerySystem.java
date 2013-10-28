79fb1de3bd63416e05c3e33bcef20b526ab0c2f3
irmaosver <irmaosver@gmail.com>
2013-10-28 17:46:36 +0000

    Added comments

--- Worldly/src/com/seggroupI/Data_Model/QuerySystem.java
+++ Worldly/src/com/seggroupI/Data_Model/QuerySystem.java
@@ -5,6 +5,7 @@ import org.json.JSONArray;
 public class QuerySystem {
 	
 	//private final String API_BASE_URL = "http://api.worldbank.org/countries/";
+	//private final String API_BASE_URL_ALL_COUNTRIES = "http://api.worldbank.org/countries?per_page=256&format=json";
 	
 	public JSONArray fetchAllCountries() {
 		
