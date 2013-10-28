895edc028fcee4c8caee028649a41bbbf6e218cb
James Bellamy <k1209605@kcl.ac.uk>
2013-10-28 16:28:14 +0000

    Adding Query System Class File

---
+++ Worldly/src/com/seggroupI/Data_Model/QuerySystem.java
@@ -0,0 +1,14 @@
+package com.seggroupI.Data_Model;
+
+import org.json.JSONArray;
+
+public class QuerySystem {
+	
+	//private final String API_BASE_URL = "http://api.worldbank.org/countries/";
+	
+	public JSONArray fetchAllCountries() {
+		
+		return null;
+	}
+
+}
