842cc4157336920b27a211dfd82a96e1bd1b6252
James Bellamy <k1209605@kcl.ac.uk>
2013-11-03 22:20:31 +0000

    rm project properties, minor change to getallcountries

--- Worldly/src/com/worldly/network/QuerySystem.java
+++ Worldly/src/com/worldly/network/QuerySystem.java
@@ -6,11 +6,12 @@ public class QuerySystem {
 	//private final String API_BASE_URL = "http://api.worldbank.org/";
 	
 	private final static String FORMAT_APPEND = "format=json";
-	private final static String GET_ALL_COUNTRIES_URL = "http://api.worldbank.org/countries?per_page=500&";
+	private final static String GET_ALL_COUNTRIES_URL = "http://api.worldbank.org/countries?per_page=500";
 	private final static String INDICATOR_BASE_URL = "http://api.worldbank.org/indicators/";
 	
 	public static String getAllCountriesData() {		
-		return JSONCore.readData(GET_ALL_COUNTRIES_URL + FORMAT_APPEND);
+		String url = GET_ALL_COUNTRIES_URL + "&" + FORMAT_APPEND;
+		return JSONCore.readData(url);
 	}
 	
 	public static String getIndicatorData(String indicatorNeeded) {
