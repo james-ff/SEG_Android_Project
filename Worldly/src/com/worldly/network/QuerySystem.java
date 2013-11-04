e4400a9f9a8258fca3150c55fc60dac713a04716
James Bellamy <k1209605@kcl.ac.uk>
2013-11-04 01:10:01 +0000

    Added missing version.xml
    Uncommented Fixed line in Manifest
    
    Added support library
    
    Minor enhancements to Query System

--- Worldly/src/com/worldly/network/QuerySystem.java
+++ Worldly/src/com/worldly/network/QuerySystem.java
@@ -6,17 +6,17 @@ public class QuerySystem {
 	//private final String API_BASE_URL = "http://api.worldbank.org/";
 	
 	private final static String FORMAT_APPEND = "format=json";
-	private final static String GET_ALL_COUNTRIES_URL = "http://api.worldbank.org/countries?per_page=500";
+	
+	private final static String GET_ALL_COUNTRIES_URL = "http://api.worldbank.org/countries";
 	private final static String INDICATOR_BASE_URL = "http://api.worldbank.org/indicators/";
 	
 	public static String getAllCountriesData() {		
-		String url = GET_ALL_COUNTRIES_URL + "&" + FORMAT_APPEND;
+		String url = GET_ALL_COUNTRIES_URL + "?" + "per_page=" + 500 + "&" + FORMAT_APPEND;
 		return JSONCore.readData(url);
 	}
 	
 	public static String getIndicatorData(String indicatorNeeded) {
 		String url = INDICATOR_BASE_URL + indicatorNeeded + "?" + FORMAT_APPEND;
-		
 		return JSONCore.readData(url);
 	}
 
